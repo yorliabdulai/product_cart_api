68c7daea75edbcf3ebff2adbdf9438dc4854a01c
yorliabdulai <yorliabdulai1@gmail.com>
2024-12-03 20:10:40 +0000

    Remove unnecessary comments from calculateTotal method in Cart model

--- models/Cart.js
+++ models/Cart.js
@@ -25,16 +25,6 @@ const CartSchema = new mongoose.Schema({
   }
 });
 
-/*************  ✨ Codeium Command ⭐  *************/
-/**
- * Calculates the total price of all products in the cart.
- * 
- * Iterates over each product in the cart and sums up the total
- * price by multiplying the product price with its quantity.
- * 
- * @returns {number} The total price of all products in the cart.
- */
-/******  93384741-b921-4775-9906-a6407d6cec5a  *******/
 CartSchema.methods.calculateTotal = function() {
   return this.products.reduce((total, item) => {
     return total + (item.product.price * item.quantity);
