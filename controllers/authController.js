f8c7667cbe68959cb4c76347492be3619aa02a88
yorliabdulai <yorliabdulai1@gmail.com>
2024-12-04 17:46:47 +0000

    Implement user registration and login functionality with JWT authentication and password hashing

---
+++ controllers/authController.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+exports.register = async (req, res, next) => {
+  try {
+    const { username, password, role } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, password: hashedPassword, role });
+    await newUser.save();
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1d' });
+    res.status(200).json({ token });
+  } catch (err) {
+    next(err);
+  }
+};
