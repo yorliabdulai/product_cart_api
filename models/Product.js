0c9b6483fcdd0d7fb5850baa09d8817e14cac46a
yorliabdulai <yorliabdulai1@gmail.com>
2024-12-04 17:49:31 +0000

    Simplify Product schema by reducing verbosity in field definitions

--- models/Product.js
+++ models/Product.js
@@ -1,30 +1,15 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  category: {
-    type: String,
-    required: true
-  },
-  price: {
-    type: Number,
-    required: true,
-    min: 0
-  },
+  name: { type: String, required: true },
+  category: { type: String, required: true },
+  price: { type: Number, required: true },
   image: {
     thumbnail: String,
     mobile: String,
     tablet: String,
-    desktop: String
+    desktop: String,
   },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  }
 });
 
 module.exports = mongoose.model('Product', ProductSchema);
