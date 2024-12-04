1173ca1ed1086da7fc6b2748dadd0047fe55ca96
yorliabdulai <yorliabdulai1@gmail.com>
2024-12-04 17:50:11 +0000

    Refactor Cart schema to simplify product representation and remove unused fields

--- models/Cart.js
+++ models/Cart.js
@@ -1,34 +1,13 @@
 const mongoose = require('mongoose');
 
 const CartSchema = new mongoose.Schema({
-  products: [{
-    product: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Product',
-      required: true
+  userId: { type: String, required: true },
+  products: [
+    {
+      productId: { type: String, required: true },
+      quantity: { type: Number, required: true },
     },
-    quantity: {
-      type: Number,
-      required: true,
-      min: 1,
-      default: 1
-    }
-  }],
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  total: {
-    type: Number,
-    default: 0
-  }
+  ],
 });
-
-CartSchema.methods.calculateTotal = function() {
-  return this.products.reduce((total, item) => {
-    return total + (item.product.price * item.quantity);
-  }, 0);
-};
 
 module.exports = mongoose.model('Cart', CartSchema);
