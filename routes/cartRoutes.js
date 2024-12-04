d6c144d4351d02818546ed2f3fa295da22a448ac
yorliabdulai <yorliabdulai1@gmail.com>
2024-12-04 17:56:17 +0000

    Refactor cart routes: replace middleware with controller functions and streamline token verification

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const router = express.Router();
-const cartController = require('../controllers/cartController');
-const authMiddleware = require('../middleware/authMiddleware');
+const { 
+  getCart, 
+  addToCart, 
+  updateCartItem, 
+  removeCartItem 
+} = require('../controllers/cartController');
+const { verifyToken } = require('../middlewares/authMiddleware');
 
-// All cart routes require authentication
-router.get('/', authMiddleware, cartController.getCart);
-router.post('/', authMiddleware, cartController.addToCart);
-router.put('/:id', authMiddleware, cartController.updateCartItem);
-router.delete('/:id', authMiddleware, cartController.removeFromCart);
+const router = express.Router();
+
+router.get('/', verifyToken, getCart);
+router.post('/', verifyToken, addToCart);
+router.put('/:id', verifyToken, updateCartItem);
+router.delete('/:id', verifyToken, removeCartItem);
 
 module.exports = router;
